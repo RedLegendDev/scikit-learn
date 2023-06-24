8d5332e575225f1de777519677fa59228356a79d
Red Dev <116916528+RedLegendDev@users.noreply.github.com>
2023-06-24 18:16:30 +0300

    Update base.py
    defaultdict

--- sklearn/base.py
+++ sklearn/base.py
@@ -1067,7 +1067,20 @@ class _UnstableArchMixin:
             )
         }
 
+tag_to_packages: dict = defaultdict(list)
+for package, (min_version, extras) in dependent_packages.items():
+    for extra in extras.split(", "):
+        tag_to_packages[extra].append("{}>={}".format(package, min_version))
 
+
+# Used by CI to get the min dependencies
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Get min dependencies for a package")
+
+    parser.add_argument("package", choices=dependent_packages)
+    args = parser.parse_args()
+    min_version = dependent_packages[args.package][0]
+    print(min_version)
 def is_classifier(estimator):
     """Return True if the given estimator is (probably) a classifier.
 
